544068fc0c68521e7873bbf416c153216d1a118c
LEO <hick317@gmail.com>
2024-06-21 20:48:06 +0530

    final change

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ async function main() {
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: "https://travel-and-stay-booking-app.vercel.app/",
+      origin: "https://travel-and-stay-booking-app.vercel.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     }),
